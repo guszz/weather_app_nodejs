eab7e33d46df98e45b8a045be3e98f9690709f16
Dilton Neves <diltonfilho@multiplix.net.br>
2019-08-07 15:32:27 -0300

    criando novas funções do app

--- public/main.js
+++ public/main.js
@@ -1,3 +1,36 @@
+// configurando Geoloacation
+$(document).ready(function () {
+  let lat;
+  let long;
+  let myPlace;
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function (position) {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      const geoCode =
+
+        $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&key=AIzaSyDSXm9iGOJQqBYzk77OSmbQkqE9NMTTQNI", function (data) {
+          myPlace = data.results[4];
+        });
+      fetch('/weather', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          latitude: lat,
+          longitude: long
+        })
+      }).then(res => res.json()).then(data => {
+        setWeatherData(data, myPlace.formatted_address)
+      })
+    })
+  else
+    console.log('geolocation is not supported')
+})
+
+// adcionando coordenadas
 const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement)
 searchBox.addListener('places_changed', () => {
@@ -17,6 +50,7 @@ searchBox.addListener('places_changed', () => {
     })
   }).then(res => res.json()).then(data => {
     setWeatherData(data, place.formatted_address)
+    closeModal()
   })
 })
 
@@ -28,11 +62,25 @@ const temperatureMaxElement = document.querySelector('[data-temperature-max]')
 icon.set('icon', 'clear-day')
 icon.play()
 
-function setWeatherData(data, place) {
+function setWeatherData(data, place, myPlace) {
+  locationElement.textContent = myPlace
   locationElement.textContent = place
   temperatureElement.textContent = Math.floor(data.currently.temperature) + ' Cº'
   temperatureMinElement.textContent = Math.floor(data.daily.data[0].temperatureMin) + ' Cº'
   temperatureMaxElement.textContent = Math.floor(data.daily.data[0].temperatureMax) + ' Cº'
   icon.set('icon', data.currently.icon)
   icon.play()
+}
+
+// configurando Modal
+document.getElementById('container').addEventListener('click', () => {
+  document.querySelector('.modal-container').style.display = 'flex'
+  document.querySelector('[data-city-search]').focus()
+})
+document.getElementById('fecha-modal').addEventListener('click', () => {
+  document.querySelector('.modal-container').style.display = 'none'
+})
+
+function closeModal() {
+  document.querySelector('.modal-container').style.display = 'none'
 }
